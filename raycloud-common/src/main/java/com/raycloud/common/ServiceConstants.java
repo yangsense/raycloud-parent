08ac15312b71daba91c52ee0be89c8c1e538e4eb
yangsen <mr_yang_sen@163.com>
2020-04-17 13:48:34 +0800

    002

---
+++ raycloud-common/src/main/java/com/raycloud/common/ServiceConstants.java
@@ -0,0 +1,11 @@
+package com.raycloud.common;
+
+/**
+ * @author houqj
+ * @date 2020-01-07 16:40
+ */
+public class ServiceConstants {
+
+    public static final String USC_SERVICE = "fast-admin";
+    public static final String AUTH_SERVICE = "fast-auth";
+}
